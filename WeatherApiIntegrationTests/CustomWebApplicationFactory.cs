bc023f604b060e3e3bbe205e78873fae585e3f42
Dinesh Sharma 3 <dinsharm1@publicisgroupe.net>
2025-10-04 15:40:23 +0530

    Integration Test of Web API

---
+++ WeatherApiIntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using WeatherTestApi;
+
+namespace WeatherApiIntegrationTests
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                // You can override services here for testing (e.g., mock DB)
+            });
+        }
+    }
+}
---
+++ WeatherApiIntegrationTests/WeatherControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+
+namespace WeatherApiIntegrationTests
+{
+    public class WeatherControllerTests : IClassFixture<CustomWebApplicationFactory>
+    {
+        private readonly HttpClient _client;
+
+        //All HTTP requests are real but no external server needed.
+        public WeatherControllerTests(CustomWebApplicationFactory customWebApplicationFactory)
+        {
+            _client = customWebApplicationFactory.CreateClient();//spins up an in-memory server.
+        }
+
+        [Fact]
+        public async Task GetWeather_ReturnsOk_WithExpectedContent()
+        {
+            // Arrange
+            var city = "London";
+
+            // Act
+            var response = await _client.GetAsync($"/WeatherForecast");
+
+            response.EnsureSuccessStatusCode(); // StatusCode 200-299
+            var content = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            content.Should().Be($"\"Sunny in {city}\"");  // Note: API returns JSON string
+        }
+    }
+}
